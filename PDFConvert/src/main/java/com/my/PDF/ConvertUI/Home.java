9feb513aa267ebdb6db530fa7a525f4b689f4b3b
mayong <>
2020-09-02 17:41:07 +0800

    保存

--- PDFConvert/src/main/java/com/my/PDF/ConvertUI/Home.java
+++ PDFConvert/src/main/java/com/my/PDF/ConvertUI/Home.java
@@ -1,19 +1,27 @@
 package com.my.pdf.convertUI;
 
+import com.my.pdf.tool.Tool;
 import javafx.application.Application;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.Toggle;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+
+import javax.xml.soap.Text;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * @author: mayong
@@ -21,20 +29,22 @@ import javafx.stage.Stage;
  */
 public class Home extends Application {
 
+    private Stage primaryStage;
+    private SelectType selectType = SelectType.FILE;
+    private ConvertType convertType = ConvertType.MERGE;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
         primaryStage.setScene(new Scene(getSceneNode()));
         primaryStage.show();
     }
 
     private Parent getSceneNode() {
         VBox rootBox = new VBox();
-        rootBox.setPadding(new Insets(8));
-        // 第一行， 选择文件、或者文件夹
-        rootBox.getChildren().add(getChooseFileOrDirectory());
-        // 第二行，选择文件
-        // 第三行，选择输出的目录
-        // 第四行，转换按钮
+        rootBox.setSpacing(20);
+        rootBox.setPadding(new Insets(20));
+        rootBox.getChildren().addAll(getChooseFileOrDirectory(), getInputFileSelect(), getOutputFileSelect(), getConvertSelect(), getStartButton());
         return rootBox;
     }
 
@@ -53,6 +63,103 @@ public class Home extends Application {
         fileButton.setUserData("FILE");
         directoryButton.setUserData("DIRECTORY");
 
+        group.selectedToggleProperty().addListener((ov, old_Toggle, new_Toggle) -> {
+            Toggle toggle = group.getSelectedToggle();
+            if (toggle != null) {
+                String userData = (String) toggle.getUserData();
+                switch (userData) {
+                    case "FILE":
+                        this.selectType = SelectType.FILE;
+                    case "DIRECTORY":
+                        this.selectType = SelectType.DIRECTORY;
+                }
+            }
+        });
+
+        Label titleLabel = new Label("选择:");
+        titleLabel.setPrefWidth(80);
+        titleLabel.setTextAlignment(TextAlignment.JUSTIFY);
+
+        HBox hBox = new HBox();
+        hBox.setSpacing(8);
+        hBox.getChildren().addAll(titleLabel, fileButton, directoryButton);
+
+        return hBox;
+    }
+
+    private Node getInputFileSelect() {
+
+        final Button inputButton = new Button("选择文件（夹）");
+        inputButton.setOnAction(event -> {
+            switch (this.selectType) {
+                case FILE:
+                    Tool
+                        .fileChooser()
+                        .filterFileNullOrExists()
+                        .extensionFilter("IMAGE FILES", new String[]{"*.jpg", "*.png", "*.jpeg"})
+                        .showMultipleOnStage(this.primaryStage, files -> {
+                            // TODO
+                            String[] filePaths = Arrays.stream(files).map(file -> file.getPath()).toArray(String[]::new);
+                            Label inputFileLabel = new Label(String.join("\r", filePaths));
+
+                            VBox rootBox = (VBox) this.primaryStage.getScene().getRoot();
+                            rootBox.getChildren().add(2, inputFileLabel);
+                        });
+                    break;
+                case DIRECTORY:
+                    Tool
+                        .directoryChooser()
+                        .filterDirectoryNullOrExists()
+                        .showOnStage(this.primaryStage, directory -> {
+                            // TODO
+                        });
+                    break;
+            }
+        });
+
+        final Label titleLabel = new Label("选择文件(夹):");
+        titleLabel.setPrefWidth(80);
+
+        final HBox hBox = new HBox();
+        hBox.setSpacing(16);
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.getChildren().addAll(titleLabel, inputButton);
+        return hBox;
+    }
+
+    private Node getOutputFileSelect() {
+
+        final TextField outputTextFiled = new TextField();
+        outputTextFiled.setPromptText("选择文件(夹)");
+        outputTextFiled.textProperty().addListener((observable, oldText, newText) -> {
+            /// 拿到了输出的文件夹目录
+            System.out.println(newText);
+        });
+
+        final Label titleLabel = new Label("选择文件(夹):");
+        titleLabel.setPrefWidth(80);
+
+        final HBox hBox = new HBox();
+        hBox.setSpacing(16);
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.getChildren().addAll(titleLabel, outputTextFiled);
+        return hBox;
+    }
+
+    private Node getConvertSelect() {
+
+        final ToggleGroup group = new ToggleGroup();
+
+        RadioButton mergeButton = new RadioButton("合成");
+        mergeButton.setToggleGroup(group);
+        mergeButton.setSelected(true);
+
+        RadioButton singleButton = new RadioButton("单张转换");
+        singleButton.setToggleGroup(group);
+
+
+        mergeButton.setUserData("MERGE");
+        singleButton.setUserData("SINGLE");
 
         group.selectedToggleProperty().addListener(
             (ObservableValue<? extends Toggle> ov, Toggle old_Toggle,
@@ -62,12 +169,48 @@ public class Home extends Application {
                 }
             });
 
-        Label titleLabel = new Label("类型:");
+        Label titleLabel = new Label("转换选项:");
+        titleLabel.setPrefWidth(80);
 
         HBox hBox = new HBox();
-        hBox.setSpacing(8);
-        hBox.getChildren().addAll(titleLabel, fileButton, directoryButton);
+        hBox.setSpacing(16);
+        hBox.getChildren().addAll(titleLabel, mergeButton, singleButton);
 
         return hBox;
+    }
+
+    private Node getStartButton() {
+        final Button startButton = new Button("转换");
+        startButton.setPrefWidth(100);
+        startButton.setPrefHeight(30);
+        startButton.setOnAction(event -> {
+            // TODO
+        });
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(16);
+        hBox.getChildren().addAll(startButton);
+        return hBox;
+    }
+
+    private enum SelectType {
+        FILE("FILE"), DIRECTORY("DIRECTORY");
+
+        private String rawValue;
+
+        SelectType(String rawValue) {
+            this.rawValue = rawValue;
+        }
+    }
+
+    private enum ConvertType {
+        MERGE("MERGE"), SINGLE("SINGLE");
+
+        private String rawValue;
+
+        ConvertType(String rawValue) {
+            this.rawValue = rawValue;
+        }
     }
 }
